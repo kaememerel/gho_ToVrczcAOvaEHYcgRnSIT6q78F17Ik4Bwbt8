70d16bd8568ea6f4433c06eb8e2089d9a1ea0955
kaememerel <kaememerel@users.noreply.github.com>
2022-05-29 13:02:12 +0200

    Added functions to the source file

--- classes/pierwszaklasa.py
+++ classes/pierwszaklasa.py
@@ -1,6 +1,11 @@
 # najprostszy sposob definiowania obiektu
+# self reprezentuje instancje klasy. Dzieki uzyciu self mozemu miec dostep do wlasciwosci i funkcji testklasy
+# tworzenie obiektu realizujemy przez tzw. konstruktor - jest to specjalna funkcja, ktora jest wykonywana, kiedy tworzymy nasz obiekt.
+# w Pythonie taka funkcje pelni metoda __init__
 class Paletka:
-    pass
+    def __init__(self, kolor):
+        self.kolor_obiektu = kolor
+        print(f"Utworzylismy obiekt o kolorze {self.kolor_obiektu}m. - ID: {id(self)}")
 # pass jest to miejsce dla funkcjonalnosci, ktora bedzie dodana pozniej
 # pass moze tez byc uzyte w ciele funkcji, ktore nic nie robi
 # przyklad:
@@ -11,7 +16,9 @@ class Paletka:
 # 1. podajemy nazwe obiektu (paletka_a)
 # 2. wywolujemy konstruktor klasy (Paletka())
 def testklasy():
-    paletka_a = Paletka()
+    paletka_a = Paletka("czerwony")
+    paletka_b = Paletka("niebieski")
+    print("*****************************")
     print(f"Obiekt typu {type(paletka_a)} zawiera od razu pewne wlasciwosci i funkcje:")
 # f-string
 # val = "Python course"
@@ -21,3 +28,28 @@ def testklasy():
 # age = 32
 # print(f"imie {name} i wiek {age}")
     print(dir(paletka_a))
+    print("*****************************")
+    print(f"Obiekt typu {type(paletka_a)} zawiera od razu pewne wlasciwosci i funkcje:")
+# f-string
+# val = "Python course"
+# print(f"Rezultat zwracany przez nasz zmienn to {val}"")
+
+# name = "Kasia"
+# age = 32
+# print(f"imie {name} i wiek {age}")
+    print(dir(paletka_a))
+    print("*****************************")
+    print(f"Obiekt typu {type(paletka_a)} zawiera od razu pewne wlasciwosci i funkcje:")
+# f-string
+# val = "Python course"
+# print(f"Rezultat zwracany przez nasz zmienn to {val}"")
+
+# name = "Kasia"
+# age = 32
+# print(f"imie {name} i wiek {age}")
+    print(dir(paletka_a))
+    print("*****************************")
+    print(f"Obiekt typu {type(paletka_b)} zawiera od razu pewne wlasciwosci i funkcje:")
+    print(dir(paletka_b))
+    print(f"Kolor dla paletka_a: {paletka_a.kolor_obiektu}")
+    print(f"Kolor dla paletka_b: {paletka_b.kolor_obiektu}")
